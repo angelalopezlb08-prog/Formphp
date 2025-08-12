bc494dd2fda659a44fd65ef422cfd4327f7ab8c6
angelalopezlb08-prog <angelalopezlb08@gmail.com>
2025-08-12 16:24:06 -0500

    Informacion.php

--- Informacion.php
+++ Informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion= $_POST ['Identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo "Los nombres y apellidos del Empleado son: ".NomApellidos;
+echo "El cargo del Empleado es: ".Cargo;
+echo "El salario del Empleado es: ".Salario;
+?>
